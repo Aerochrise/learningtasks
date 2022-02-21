78579ac84eca4282cdc7a73c281390b17412d343
Aerochrise <100113490+Aerochrise@users.noreply.github.com>
2022-02-21 16:42:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
        <h1>Welcome To My Website! I am Aerochrise Kim!</h1>
        <ul>
            <li><a href="index.php">HOME</a></li>
-           <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
+           <li><a href="Homepage.html">LEARNING TASK 2</a></li>
        </ul>
        </body>
 </html>       
